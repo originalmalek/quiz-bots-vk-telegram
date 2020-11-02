5bb724d2260381ada9ee40695bef5486e8cf0c1d
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-11-02 15:09:39 +0300

    Change variables for heroku

--- telegram_logger.py
+++ telegram_logger.py
@@ -7,9 +7,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-# telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
-telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-telegram_token = os.getenv('TELEGRAM_TOKEN')
+telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
+telegram_token = os.environ['TELEGRAM_TOKEN']
 bot = telebot.TeleBot(telegram_token)
 
 class MyLogsHandler(logging.Handler):
