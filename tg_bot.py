0d908441a268564c854b1b7fc4d77ce3429cdadc
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-11-03 13:23:41 +0300

    Delete func seng_message_tg()

--- tg_bot.py
+++ tg_bot.py
@@ -9,15 +9,10 @@ from telegram_logger import MyLogsHandler
 from get_questions import create_questions_and_answer_dict
 from random import randint
 
-
 telegram_access_token = os.getenv('TELEGRAM_TOKEN')
 tg_bot = telebot.TeleBot(telegram_access_token)
 
 logger = logging.getLogger('TG')
-
-
-def send_message_tg(text, chat_id):
-    tg_bot.send_message(chat_id, text)
 
 
 def show_buttons(chat_id):
@@ -34,53 +29,54 @@ def show_buttons(chat_id):
 @tg_bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     redis.set(f'tg-{message.chat.id}', 0)
-    send_message_tg('Это бот викторина. Играй бесплатно.', message.chat.id)
+    tg_bot.send_message(message.chat.id, 'Это бот викторина. Играй бесплатно.')
     show_buttons(message.chat.id)
 
 
 @tg_bot.message_handler(regexp='новый вопрос')
 def send_question(message):
     if int(redis.get(f'tg-{message.chat.id}')) > 0:
-        send_message_tg('У вас есть не отвеченный вопрос. Для продолжения введите ответ или нажмите кнопку "Сдаться".',
-                        message.chat.id)
+        tg_bot.send_message(message.chat.id,
+                            'У вас есть не отвеченный вопрос. Для '
+                            'продолжения введите ответ или нажмите кнопку "Сдаться".')
         return None
     questions_id = randint(1, len(questions))
     redis.set(f'tg-{message.chat.id}', questions_id)
-    send_message_tg(questions[questions_id]['question'], message.chat.id)
+    tg_bot.send_message(message.chat.id,questions[questions_id]['question'])
 
 
 @tg_bot.message_handler(regexp='сдаться')
 def send_answer(message):
     questions_id = int(redis.get(f'tg-{message.chat.id}'))
     if questions_id == 0:
-        send_message_tg('У вас нет активного вопроса. Нажмите "Новый вопрос"', message.chat.id)
+        tg_bot.send_message(message.chat.id, 'У вас нет активного вопроса. Нажмите "Новый вопрос"')
         return None
     redis.set(f'tg-{message.chat.id}', 0)
     answer = questions[questions_id]['answer']
-    send_message_tg(f'Правильный ответ:\n{answer}', message.chat.id)
+    tg_bot.send_message(message.chat.id, f'Правильный ответ:\n{answer}')
 
 
 @tg_bot.message_handler(regexp='мой счет')
 def send_scores(message):
-    send_message_tg('Ваш счет: 777. Вы молодец!', message.chat.id)
+    tg_bot.send_message(message.chat.id, 'Ваш счет: 777. Вы молодец!')
 
 
 @tg_bot.message_handler(content_types='text')
 def check_answer(message):
     questions_id = int(redis.get(f'tg-{message.chat.id}'))
     if questions_id == 0:
-        send_message_tg('У вас нет активного вопроса! Нажмите "Новый вопрос".', message.chat.id)
+        tg_bot.send_message(message.chat.id, 'У вас нет активного вопроса! Нажмите "Новый вопрос".')
         return None
     answer = questions[questions_id]['answer'].split(sep='.')[0]
     if len(message.text) <= 3:
-        send_message_tg('Ответ неверный, попробуйте еще раз.', message.chat.id)
+        tg_bot.send_message(message.chat.id, 'Ответ неверный, попробуйте еще раз.')
         return None
     if message.text.lower() in answer.lower():
         redis.set(f'tg-{message.chat.id}', 0)
-        send_message_tg(f'Вы угадали!\nТочный ответ: {answer}.\nДля следующего вопроса нажмите "Новый вопрос:"',
-                        message.chat.id)
+        tg_bot.send_message(message.chat.id,
+                            f'Вы угадали!\nТочный ответ: {answer}.\nДля следующего вопроса нажмите "Новый вопрос:"')
     else:
-        send_message_tg(text='Ответ неверный, попробуйте еще раз.', chat_id=message.chat.id)
+        tg_bot.send_message(message.chat.id, 'Ответ неверный, попробуйте еще раз.')
 
 
 if __name__ == '__main__':
@@ -88,6 +84,7 @@ if __name__ == '__main__':
     redis_host = os.getenv('REDIS_HOST')
     redis_pass = os.getenv('REDIS_PASS')
     redis_port = os.getenv('REDIS_PORT')
+
     logging.basicConfig(level=20)
     logger.addHandler(MyLogsHandler())
 
