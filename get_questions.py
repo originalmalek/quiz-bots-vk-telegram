378945386321105b79bac8ed9e2c08a0812e9041
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-11-03 16:06:57 +0300

    Fix problem with questions
    I had mistake in my code and I got questions from last file. Now the problem fixed

--- get_questions.py
+++ get_questions.py
@@ -3,6 +3,7 @@ import os
 
 
 def create_questions_and_answer_dict():
+    files_text = ''
     regex_questions = r"(?<=Вопрос\D\d\:\n).*?(?=\nОтвет)|(?<=Вопрос\D\d\d:\n).*?(?=\nОтвет)" \
                       r"|(?<=Вопрос\D\d\d\d:\n).*?(?=\nОтвет)"
     regex_answers = r'(?<=Ответ:\n).*?(?=\n)'
@@ -13,9 +14,10 @@ def create_questions_and_answer_dict():
     for file_name in file_names:
         with open(f'questions/{file_name}', 'r', encoding='KOI8-R')as file:
             file_text = file.read()
+            files_text = files_text + file_text
 
-        questions = re.finditer(regex_questions, file_text, re.MULTILINE | re.DOTALL)
-        answers = re.finditer(regex_answers, file_text, re.MULTILINE | re.DOTALL)
+        questions = re.finditer(regex_questions, files_text, re.MULTILINE | re.DOTALL)
+        answers = re.finditer(regex_answers, files_text, re.MULTILINE | re.DOTALL)
 
     for id_question, (question, answer) in enumerate(zip(questions, answers), start=1):
 
@@ -24,3 +26,4 @@ def create_questions_and_answer_dict():
                                      'answer': answer.group()}})
 
     return questions_and_answer
+
